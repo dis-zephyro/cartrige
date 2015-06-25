252a41db6828a4992403655db5e5bdde59e28c5f
dis-zephyro <zephyro@yandex.ru>
2015-06-25 12:03:04 +0300

    vq

--- js/main.js
+++ js/main.js
@@ -21,42 +21,95 @@ ymaps.ready(init);
 
 function init () {
     var myMap1 = new ymaps.Map("map1", {
-        center: [56.3112,44.0038],
-        zoom: 14,
-        controls: []
+            center: [56.3112,44.0038],
+            zoom: 14,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark1 = new ymaps.Placemark([56.3112,44.0038], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "<strong>Нижний Новгород, Тверская,17. оф.15</strong>",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки"
+        });
+
+    myMap1.geoObjects.add(myPlacemark1);
+    myMap1.behaviors.disable('scrollZoom');
+
+    // Открываем балун на карте (без привязки к геообъекту).
+    myMap1.balloon.open([56.3112,44.0038], "<strong class='map_address'>Нижний Новгород,<br/>Тверская,17. оф.15</strong>", {
+        // Опция: не показываем кнопку закрытия.
+        closeButton: false
     });
 
     var myMap2 = new ymaps.Map("map2", {
-        center: [56.3112,44.0038],
-        zoom: 14,
-        controls: []
+            center: [56.3112,44.0038],
+            zoom: 14,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark2 = new ymaps.Placemark([56.3112,44.0038], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "<strong class='map_address'>Нижний Новгород,<br/>Тверская,17. оф.15</strong>",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки"
+        });
+
+    myMap2.geoObjects.add(myPlacemark2);
+    myMap2.behaviors.disable('scrollZoom');
+
+    // Открываем балун на карте (без привязки к геообъекту).
+    myMap2.balloon.open([56.3112,44.0038], "<strong class='map_address'>Нижний Новгород,<br/>Тверская,17. оф.15</strong>", {
+        // Опция: не показываем кнопку закрытия.
+        closeButton: false
     });
 
-    myMap1.geoObjects
-        .add(new ymaps.Placemark([56.3112,44.0038], {
-            balloonContent: 'Нижний Новгород, Тверская,17. оф.15'
+}
+/*
+ymaps.ready(init);
+
+function init () {
+    var myMap1 = new ymaps.Map("map1", {
+            center: [56.3112,44.0038],
+            zoom: 14,
+            controls: []
         }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/placemark.png',
-            iconImageSize: [45, 68],
-            iconImageOffset: [-22, -69]
-        }));
+            searchControlProvider: 'yandex#search'
+        }),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [56.3112,44.0038]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Нижний Новгород, Тверская,17. оф.15',
+                hintContent: 'Нижний Новгород, Тверская,17. оф.15'
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        });
+
+    myMap1.geoObjects
+        .add(myGeoObject);
 
     myMap1.behaviors.disable('scrollZoom');
 
-    myMap2.geoObjects
-        .add(new ymaps.Placemark([56.3112,44.0038], {
-            balloonContent: 'Нижний Новгород, Тверская,17. оф.15'
-        }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/placemark.png',
-            iconImageSize: [45, 68],
-            iconImageOffset: [-22, -69]
-        }));
-
-    myMap2.behaviors.disable('scrollZoom');
 }
 
+*/
 
 
 function timer() //в блоке таймера дописывайте содежрание блоков '00' !!!! тоесть <div class="day">00</div>
